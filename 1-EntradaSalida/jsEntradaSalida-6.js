36f80c1726846186eb270b2f65ec94de0e38c33e
alumno <alumno>
2019-02-08 10:31:26 -0300

    modifique el archivo javascript numero 6

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,16 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var PrimerNumero;
+	var SegundoNumero;
+
+	PrimerNumero=numeroUno.value;
+	SegundoNumero=numeroDos.value;
+
+	PrimerNumero=parseInt(PrimerNumero);
+	SegundoNumero=parseInt(SegundoNumero);
+	
+	alert(PrimerNumero + SegundoNumero);
 
 }
 
