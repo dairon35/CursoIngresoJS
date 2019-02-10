2265212b4605aa5980a0a07c2b2b9c22e55426fc
unknown <daironalvarado35@gmail.com>
2019-02-10 14:36:03 -0300

    resuelto entrada y salida completo

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,21 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	var ingreseImporte;
+	var operacion;
+	var operacion1;
+
+	ingreseImporte=document.getElementById('importe').value;
 	
+	ingreseImporte=parseInt(ingreseImporte);
+
+	//operacion = ingreseImporte - (ingreseImporte * 25 / 100 ); SOLUCION DIFERENTE
+
+	//resultado.value = operacion; SOLUCION DIFERENTE
+	
+	operacion=(ingreseImporte * 0.25);
+
+	operacion1=(ingreseImporte - operacion);
+
+	document.getElementById('resultado').value=operacion1;
 }
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,77 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primerNumero;
+	var segundoNumero;
+	var operacion;
+
+	primerNumero = document.getElementById('numeroUno').value;
+	segundoNumero = document.getElementById('numeroDos').value;
+
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = parseInt(segundoNumero);
+
+	operacion = primerNumero + segundoNumero;
+
+	alert(operacion);
 }
+
+
+
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var operacion;
+
+	primerNumero = document.getElementById('numeroUno').value;
+	segundoNumero = document.getElementById('numeroDos').value;
+
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = parseInt(segundoNumero);
+
+	operacion = primerNumero - segundoNumero;
+
+	alert(operacion);
 }
+
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var operacion;
+
+	primerNumero = document.getElementById('numeroUno').value;
+	segundoNumero = document.getElementById('numeroDos').value;
+
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = parseInt(segundoNumero);
+
+	operacion = primerNumero * segundoNumero;
+
+	alert(operacion);
 }
+
 
 function dividir()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var operacion;
+
+	primerNumero = document.getElementById('numeroUno').value;
+	segundoNumero = document.getElementById('numeroDos').value;
+
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = parseInt(segundoNumero);
+
+	operacion = primerNumero / segundoNumero;
+
+	alert(operacion);
 }
+
+
+
 
