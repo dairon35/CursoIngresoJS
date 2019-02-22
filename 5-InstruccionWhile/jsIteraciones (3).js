6df0c9226da6258006879ffccce7a96a5fd9203d
dairon35 <daironalvarado35@gmail.com>
2019-02-22 12:20:46 -0300

    WHILE 2  y 3 RECUPERATORIO EJERCICIOS DE PIZARRA

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,20 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+var clave; 
+var cont=0;
+
+	clave= prompt("ingrese el número clave.");
+
+	while(clave !="utn750")
+	{
+		clave=prompt("ingrese el número clave.");
+
+		if (clave !="utn750" && cont < 4)
+		{
+			alert("supero el limite de intentos");
+		}
+	}
 
 
 }//FIN DE LA FUNCIÓN
