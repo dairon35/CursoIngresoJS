fb8f15d2514aa4d636489f46e317c7bd0a0f5409
alumno <alumno>
2019-02-28 11:56:53 -0300

    while

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,25 @@
 function mostrar()
-{
+{   /*evaluar en -2 -5 3 8 0*/
+	var contador;
+	var respuesta;
+	var sumaPositivos;
+	var sumaNegativos;
+	var numero;
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
+	respuesta="si";
+	sumaNegativos=0;
 
 	while(respuesta!="no")
 	{
-		
-	
+		numero=prompt("ingrese un numero: ");
+		numero=parseInt(numero);
+
+		if (numero<0)
+		{
+			sumaNegativos=sumaNegativos+numero;
+		}
+		respuesta=prompt("ingrese 'no' para salir "," escriba aqui");
 	}
-
-
-
+		console.log("suma negativos es igual a: "+sumaNegativos);
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -6,12 +6,17 @@ function mostrar()
 	var numeroMinimo;
 	var numeroIngresado;
 	var respuesta;
+	var bandera;
 
 	
 	// declarar variables
 	contador=0;
 	respuesta='si';
-	numeroMaximo=-999;
+	//numeroMaximo=-999;
+	//numeroMinimo=999;
+	bandera=0;
+
+
 
 
 	while(respuesta!='no')
@@ -19,14 +24,30 @@ function mostrar()
 		numeroIngresado=prompt("Ingrese Numero");
 		numeroIngresado=parseInt(numeroIngresado);
 
-		if (numeroIngresado>numeroMaximo)
-		 {
-		 	numeroMaximo=numeroIngresado;
-		 }
-		respuesta=prompt("Ingrese no para salir");
-	
-	}
-			console.log("el maximo numero es "+numeroMaximo);
+	if (bandera==0)
+	{
+		numeroMaximo=numeroIngresado;
+		numeroMinimo=numeroIngresado;
+		bandera=1;
+	}	else()
+		{
+			if (numeroIngresado>numeroMaximo)
+			 {
+			 	numeroMaximo=numeroIngresado;
+			 }
+			
+			if (numeroIngresado>numeroMinimo)
+			{
+				numeroMinimo=numeroIngresado
+			}
+
+				}
+
+			 respuesta=prompt("Ingrese no para salir");
+						
+		}
+				console.log("el maximo numero es "+numeroMaximo);
+				console.log("el maximo numero es "+numeroMinimo);
 
 
 
