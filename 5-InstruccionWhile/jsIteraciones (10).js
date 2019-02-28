4a4d8027daca031c2eb4a69b15536aa9159ab1ad
alumno <alumno>
2019-02-28 12:12:03 -0300

    while 10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,6 +1,7 @@
 function mostrar()
 {   /*evaluar en -2 -5 3 8 0*/
-	var contador;
+	var contadorPositivos;
+	var contadorNegativos;
 	var respuesta;
 	var sumaPositivos;
 	var sumaNegativos;
@@ -8,6 +9,7 @@ function mostrar()
 
 	respuesta="si";
 	sumaNegativos=0;
+	sumaPositivos=0;
 
 	while(respuesta!="no")
 	{
@@ -18,8 +20,18 @@ function mostrar()
 		{
 			sumaNegativos=sumaNegativos+numero;
 		}
+		else
+		{
+			if (numero >0)
+			{
+				sumaPositivos=sumaPositivos+numero
+			}
+		}
+		else
+			if (numero) {}
 		respuesta=prompt("ingrese 'no' para salir "," escriba aqui");
 	}
 		console.log("suma negativos es igual a: "+sumaNegativos);
+		console.log("suma positivos es igual a: "+sumaPositivos);
 
 }//FIN DE LA FUNCIÓN
