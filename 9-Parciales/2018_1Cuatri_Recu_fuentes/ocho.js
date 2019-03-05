1d72806db2f3e0bc8066b4ebd954e09aaf9d74d1
unknown <daironalvarado35@gmail.com>
2019-03-05 19:10:02 -0300

    resuelto 7 y 8 de parcial

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,100 @@
 function mostrar()
 {
+	var checkConfirm = true;
+	var numero:
+	var letra;
+	var cantidadPares=0;
+	var cantidadImpares=0;
+	var cantidadCeros=0;
+	var CantidadPositivo;
+	var PromedioPositivos;
+	var sumaNegativos=0;
+	
 
-}
+	}
+	/*var checkConfirm = true;
+	var nombrePaisOk;
+	var nombrePais;
+	var extensionTerritorial;
+	var contadorPais = 0;
+	var acumuladorExtensiones = 0; 
+	var superficieIgualCien = 100;
+	var contadorPar = 0;
+	var contadorPaisMenor = 0;
+	var contadorPaisCien = 0;
+	var fisrtName = '';
+	var firstCountry = true;
+	var promedio = 0;
+	var menorTerritorio = 0;
+	var nombreMenorTerritorio = '';
+	var territorioComparacion = true;
+
+	
+	
+	
+
+	while(checkConfirm) {
+		nombrePaisOk = false;
+
+		while(!nombrePaisOk) {
+			 nombrePais = prompt("Ingrese el nombre del país:");
+
+			if (typeof nombrePais === 'string' && nombrePais.length > 3) {
+				nombrePaisOk = true;
+			} else {
+				alert("Debe ingresar un nombre de pais valido");
+			}
+		}
+
+		var superficieOK = false;
+
+		while(!superficieOK) {
+			extensionTerritorial = prompt("Ingrese la extensión territorial");
+			extensionTerritorial = parseInt(extensionTerritorial);
+
+			if (typeof extensionTerritorial === 'number' && extensionTerritorial > 0) {
+				acumuladorExtensiones += extensionTerritorial;
+
+				if (territorioComparacion) {
+					menorTerritorio = extensionTerritorial;
+					territorioComparacion = false;
+				}
+
+				superficieOK = true;
+			} else {
+				alert("Debe ingresar una extensión válida");
+			}
+		}
+
+		if (extensionTerritorial % 2 != 0) {
+			contadorPar++;
+		}
+
+		if (extensionTerritorial < superficieIgualCien) {
+			contadorPaisMenor++;
+		}
+
+		if (extensionTerritorial === superficieIgualCien) {
+			contadorPaisCien++;
+		}
+
+		if (extensionTerritorial > 100 && firstCountry) {
+			fisrtName = nombrePais;
+			firstCountry = false;
+		}
+
+		if (extensionTerritorial < menorTerritorio) {
+			menorTerritorio = extensionTerritorial;
+			nombreMenorTerritorio = nombrePais;
+		}
+
+		contadorPais++;
+
+		checkConfirm = confirm("¿Desea continuar?");
+		
+	}
+
+	promedio = acumuladorExtensiones / contadorPais;
+
+	document.write("la cantidad de paises con superficie impar son: " +contadorPar+ " la cantidad de paises con superficie menor a 100 son: " +contadorPaisMenor+ " la cantidad de paises con superficie igual a 100 son: " +contadorPaisCien+ " El nombre del primer pais que supero los 100: " +fisrtName+ " el promedio de KM imgresados es de: " +promedio+ " el nombre del que menos territorio tiene es: " +nombreMenorTerritorio);*/
+
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,132 @@
 function mostrar()
 {
+	var nota=0;
+	var sexo;
+	var contador=0;
+	var promedio;
+	var acumuladorNotas=0;
+	var notaVaronesSeis;
+	var notaBaja=10;
+	var sexoNotaBaja;
+	var notaVarones;
+	var sexoNotaVarones;
+	var contadorVarones=0;
+
+	while(contador<5)
+	{
+		var notaOk = false;
+
+		while(!notaOk){
+
+		nota = prompt("Por favor Ingrese su nota: ");
+		nota = parseInt(nota);
+
+		if (nota>0 && nota<11){
+
+			acumuladorNotas += nota;
+			notaOk=true;
+			
+			}
+			else{
+					alert("La nota debe estar  en el rango 0 a 10");
+
+				}	
+			}
+			var sexoOk= false;
+
+			while(!sexoOk){
+
+				sexo=prompt("por favor ingrese f si su sexo es femenino o m si su sexo es masculino");
+
+				if (sexo == "f" || sexo== "m" ){
+
+					sexoOk=true;
+				}
+				if (nota < notaBaja)
+				{
+					notaBaja=nota;
+					sexoNotaBaja=sexo;
+				}
+				if (nota>=6 && sexo== "m"){
+					contadorVarones++;
+
+				}
+
+			}
+
+
+		contador++;
+	}
+
+	promedio=acumuladorNotas /contador;
+
+	alert("el promedio de las 5  notas es de: " +promedio+ " la nota mas baja es de: " +notaBaja+ " el sexo de la persona con nota mas baja es: " +sexoNotaBaja+ " La cantidad de varones que su nota haya sido mayor o igual a 6 : " +contadorVarones);
 
 }
+
+
+
+
+
+
+
+
+	/*var velocidadOK;
+	var velocidad = 0;
+	var cont = 0;
+	var combustible = '';
+	var acumVelocidad = 0;
+	var cont = 0;
+	var promedioVelocidad = 0;
+	var velocidadMinima = 250;
+	var velocidadMinimaCombustible = '';
+	var contLiquidoMaxVelocidad = 0;
+	var velocidadMaxSolido = '0';
+
+	while(cont<5)
+	{
+		velocidadOK = false;
+		
+		while (!velocidadOK) {
+			velocidad = prompt("Por favor ingrese la velocidad del vehiculo: ");
+			velocidad = parseInt(velocidad);
+
+			if (velocidad > 0 && velocidad < 251) {
+				
+				acumVelocidad += velocidad;
+				velocidadOK = true;
+			} else {
+				alert("El rango de velocidad solo puede ser de 0 a 250 km/h");
+			}
+		}
+
+		var naftaOK = false;
+
+		while(!naftaOK) {
+			combustible = prompt("Por favor ingrese el tipo de combustible si es solido 's' o liquido 'l'");
+
+			if (combustible == 's' || combustible == 'l') {
+				naftaOK = true;
+			}
+		}
+
+		if (velocidad < velocidadMinima) {
+			velocidadMinima = velocidad;
+			velocidadMinimaCombustible = combustible;
+		}
+
+		if (combustible == 'l' && velocidad > 100) {
+			contLiquidoMaxVelocidad++;
+		}
+
+		if (velocidad > velocidadMaxSolido && combustible == 's') {
+			velocidadMaxSolido = velocidad;
+		}
+
+		cont++;
+	}
+
+	promedioVelocidad = acumVelocidad / cont;
+
+	alert("El promedio de velocidad es de: " + promedioVelocidad + " velocidad minima es de: " + velocidadMinima + " El tipo de combustible de la velocidad mas baja es: " + velocidadMinimaCombustible + " la cantindad de combustible liquidos que superaron los 100 km son: " + contLiquidoMaxVelocidad + " la velocidad mas alta de los combustibles solidos es de: " + velocidadMaxSolido);*/
+
